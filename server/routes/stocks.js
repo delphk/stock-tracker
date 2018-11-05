8803835fad128a0c88d4940198080812df04a670
delphinekoh <delphinekoh@hotmail.com>
2018-11-05 16:17:55 +0800

    edit app.js and stocks.js route

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = require("express-promise-router")();
+const router = express.Router();
 const passport = require("passport");
 const Stock = require("../models/stock");
 
